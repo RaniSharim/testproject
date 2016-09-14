710bd32f12a05cdd66bf2eb5303da47bbe5c09f5
rsharim <rani.sharim@gmail.com>
2016-09-14 23:25:38 +0300

    with new fucked up file : routes/offermanagement.js

--- app.js
+++ app.js
@@ -1,19 +1,6 @@
-var modul = require('./folder/testmod.js');
-var EventEmitter = require('events').EventEmitter;
-var util = require('util');
+var officemanagement = require('./routes/officemanagement');
 
-function Master() {
-   EventEmitter.call(this);
-};
 
-// Declare that your class should use EventEmitter as its prototype.
-// This is roughly equivalent to: Master.prototype = Object.create(EventEmitter.prototype)
-util.inherits(Master, EventEmitter);
 
-var myEmitter = new Master();
-
-myEmitter.on('event', function() { modul.func(); });
-
-setTimeout(function() {
-    myEmitter.emit('event');
-}, 1000);
+officemanagement.ShowBranchEntriesForBranch
+();
---
+++ routes/officemanagement.js
@@ -0,0 +1,22 @@
+﻿exports.ShowBranchEntriesForBranch = function (req, res) {
+    branch = req.user.Branch;
+    var canSeeForwards = true;
+    var canChangeForwards = true;
+    var canSetStatus = true;
+    var canSetStatusToUnmanaged = false;
+    var canSetStatusBackToManager = false;
+    var canSeeAllBranches = false;
+    var canSendPriceOffers = false;
+
+    var title = "ניהול אולם ";
+    title += req.user.DisplayName;
+
+     promise.join(logic.GetTodaysEntriesByBranchForBranch(branch),
+                 usersLogic.GetSalesInBranch(branch),
+                 function(results, forwards) {
+                        formatDates(results);
+                        asyncRenderer.render(res, { title: title,  branches:[], forwards: forwards, entries: results , showForwards: canSeeForwards, canChangeForwards:canChangeForwards, canSetStatus:canSetStatus, canSetStatusToUnmanaged:canSetStatusToUnmanaged,canSetStatusBackToManager:canSetStatusBackToManager,  canSeeAllBranches:canSeeAllBranches, canSendPriceOffers:canSendPriceOffers  , currentBranch: branch});
+                 }
+    )
+}
+
