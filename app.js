7b07d446ce4b510d3f6b4bdd535b6d4b08eb6950
rsharim <rani.sharim@gmail.com>
2016-09-14 22:52:07 +0300

    trying with emitters

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 var modul = require('./folder/testmod.js');
+var EventEmitter = require('events').EventEmitter;
+var util = require('util');
 
-var f=function() {
-    modul.func();
-}
+function Master() {
+   EventEmitter.call(this);
+};
+
+// Declare that your class should use EventEmitter as its prototype.
+// This is roughly equivalent to: Master.prototype = Object.create(EventEmitter.prototype)
+util.inherits(Master, EventEmitter);
+
+var myEmitter = new Master();
+
+myEmitter.on('event', function() { modul.func(); });
 
 setTimeout(function() {
-    f();
+    myEmitter.emit('event');
 }, 1000);
